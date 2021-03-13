f433a7f03211904287e120a324daafaa36482594
shelldi <79559492+shelldi@users.noreply.github.com>
2021-03-14 00:13:53 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 api_id = 1086018  # Get it from my.telegram.org
 api_hash = "3c2f1a043c1a22d5b0af74b8268993d5"   # Get it from my.telegram.org
-feed_url = "https://torrentgalaxy.to/rss?magnet&user=29","https://torrentgalaxy.to/rss?magnet&user=6244"  # RSS Feed URL of the site.
+feed_url = "https://torrentgalaxy.to/rss?magnet&user=29"  # RSS Feed URL of the site.
 bot_token = "1668902665:AAH5Nip5RlqEEZU5PdRK37UTizMtqJYM9SQ"   # Get it by creating a bot on https://t.me/botfather
 log_channel = "-1001407732014"   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
 check_interval = 2   # Check Interval in seconds.    
