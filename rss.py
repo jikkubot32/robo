a4805d851b36f76aaffe23071f5cfdaa3d351e4d
shelldi <79559492+shelldi@users.noreply.github.com>
2021-03-24 15:11:10 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -26,7 +26,7 @@ if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna co
 db = pickledb.load('rss.db', True)
 if db.get("feed_url") == None:
   db.set("feed_url", "*")
-app = Client(session_name=session_name, api_id=api_id, api_hash=api_hash)
+app = Client(':memory:', api_id=api_id, api_hash=api_hash)
 
 def check_feed():
     FEED = feedparser.parse(feed_url)
