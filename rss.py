bdf78c8813e475e24e1a3d50a0ce193a22795a37
shelldi <79559492+shelldi@users.noreply.github.com>
2021-03-24 14:28:02 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -7,26 +7,26 @@ from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
 
 session_name = ""
-api_id = "3751415"   # Get it from my.telegram.org
-api_hash = "6547d1aaebb53eea6707945a48c4dd2f"   # Get it from my.telegram.org
-feed_url = []   # RSS Feed URL of the site.
+api_id = ""   # Get it from my.telegram.org
+api_hash = ""   # Get it from my.telegram.org
+feed_url = ["https://torrentgalaxy.to/rss?magnet"]   # RSS Feed URL of the site.
    # Get it by creating a bot on https://t.me/botfather
 log_channel = ""   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
-check_interval = 3 # Check Interval in seconds.  
-max_instances = 1  # Max parallel instance to be used.
+check_interval = 5  # Check Interval in seconds.  
+max_instances = 5   # Max parallel instance to be used.
 if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna configure the bot via env vars.
   session_name= os.environ.get("SESSION_NAME")
   api_id = os.environ.get("APP_ID")
   api_hash = os.environ.get("API_HASH")
   feed_url = os.environ.get("FEED_URL")
   log_channel = int(os.environ.get("LOG_CHANNEL", None))
-  check_interval = int(os.environ.get("INTERVAL", 3))
-  max_instances = int(os.environ.get("MAX_INSTANCES", 2))
+  check_interval = int(os.environ.get("INTERVAL", 30))
+  max_instances = int(os.environ.get("MAX_INSTANCES", 5))
 
 db = pickledb.load('rss.db', True)
 if db.get("feed_url") == None:
   db.set("feed_url", "*")
-app = Client('my_account', api_id=api_id, api_hash=api_hash)
+app = Client(session_name=session_name, api_id=api_id, api_hash=api_hash)
 
 def check_feed():
   for furl in feed_url:
